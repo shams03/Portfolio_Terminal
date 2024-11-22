d3ee517f6e8f8f89b1a8f9768095e55d83c1ace8
shams03 <95904933+shams03@users.noreply.github.com>
2024-11-22 06:06:34 +0530

    fixed the mobile verison issue - still incompatible with mobiles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
 			<div className="h-screen w-full flex justify-center items-center font-josefin">
 				<div className="w-full">
 					<div className="block sm:hidden mb-10 bg-red-200 py-3  text-center text-xl text-gray-700">
-						<p>This is designed for mobile view yet !!</p>
+						<p>This isn't designed for mobile view yet !!</p>
 					</div>
 					<div className="hidden sm:block">
 						<TerminalBox />
